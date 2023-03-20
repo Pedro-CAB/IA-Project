e6aae11bf6c92bac394c4b79f0dbeec70f9d1813
Pedro-PFerreira <up202004986@edu.fe.up.pt>
2023-03-20 11:07:19 +0000

    Started implementing the MiniMax algorithm

--- tree.py
+++ tree.py
@@ -19,11 +19,11 @@ class Piece:
 
 class Node:
     
-    def __init__(self, id, value, depth):
+    def __init__(self, id, value):
         self.id = id
         self.value = value
         self.visited = False
-        self.depth = depth
+        self.depth = 0
         self.edges = []
         
     def set_depth(self, depth):
@@ -32,12 +32,16 @@ class Node:
     def set_value(self, value):
         self.value = value
         
-    def addEdge(self, edge):
-        self.edges.add(edge)
+    def addEdge(self, dest):
+        dest.set_depth(self.depth + 1)
+        self.edges.append(dest)
+        
+    def allEdges(self):
+        return self.edges
         
     def printEdges(self):
         for edge in self.edges:
-            print(edge)
+            print((self.id, edge.id))
             
     def isEmpty(self):
         return len(self.edges) == 0
@@ -47,17 +51,70 @@ class Node:
 class Tree:
     
    def __init__(self):
-       self.nodes = {}
+       self.nodes = set()
        self.edges = []
        
 
    def addNode(self, node):
        self.nodes.add(node)
+       
+   def addEdge(self, s, d):
+       if s in self.nodes and b in self.nodes:
+           s.addEdge(d)
+           return 0
+       else:
+           return -1
+       
+   def printAllEdges(self):
+       for n in self.nodes:
+           n.printEdges()
+       
+        
+t = Tree()
+
+a = Node(1,10)
+
+b = Node(2,40)
+
+c = Node(3,45)
 
 
-def minimax(node, depth, maxPlayer):
+t.addNode(a)
+t.addNode(b)
+t.addNode(c)
+
+t.addEdge(a, b)
+t.addEdge(a, c)
+t.addEdge(b, c)
+
+t.printAllEdges()
+
+
+def minimax(node, depth, alpha, beta, maximising, player, maxPlayer, eval_func):
     if depth == 0 or node.isEmpty():
-        return node.value
+        return eval_func(node) * (1 if player == 'A' else -1)
+    
+    if maximising:
+        max_eval = float('-inf')
+        for move in node.allEdges():
+            new_node = 1 #TODO Usar o move (o outro nó)
+            evaluate = minimax(new_node, depth-1, alpha, beta, False, player, eval_func)
+            max_eval = max(alpha, evaluate)
+            alpha = max(alpha, evaluate)
+            if beta <= alpha:
+                break
+            return max_eval
 
-
-
+    #Minimimsing (opponent's turn)
+    else:
+        min_eval = float('inf')
+        for move in node.allEdges():
+            new_node = 2 #TODO
+            evaluate = minimax(new_node, depth-1, alpha, beta, True, player, eval_func)
+            min_eval = min(beta, evaluate)
+            beta = min(beta, evaluate)
+            
+            if beta <= alpha:
+                break
+            return min_eval
+            
